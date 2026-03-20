3f23027d3e8f3371c4a38ce1dc719961de17ee66
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2026-03-20 14:24:49 +0800

    feat: removed strict email validation

--- lib/validations/password.ts
+++ lib/validations/password.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const passwordSchema = z.object({
   platform: z.string().min(1, "Platform is required."),
   link: z.string().url("Please enter a valid URL.").or(z.literal("")),
-  user_email: z.string().email("Invalid email.").or(z.literal("")),
+  user_email: z.string().optional(),
   password: z.string().min(1, "Password is required."),
 });
 
